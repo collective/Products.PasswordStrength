1dd43ba29eefd29362137ecc030b8f6c4a226b3c
Stephan Geulette <s.geulette@imio.be>
2014-06-27 11:31:36 +0200

    Pep8 correction

--- Products/PasswordStrength/plugin.py
+++ Products/PasswordStrength/plugin.py
@@ -7,39 +7,24 @@ __author__ = "Dylan Jay <software@pretaweb.com>"
 
 import logging
 
-from urllib import quote, unquote
-
-from BTrees.OOBTree import OOBTree
-from DateTime import DateTime
-from AccessControl import ClassSecurityInfo, Permissions
-from AccessControl.Permissions import view
-from AccessControl import AuthEncoding
+from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from OFS.Cache import Cacheable
-from OFS.Folder import Folder
-
-from zExceptions import Unauthorized
 
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.permissions import ManageUsers
 from Products.PluggableAuthService.utils import classImplements
-from Products.PluggableAuthService.interfaces.plugins import \
-    IValidationPlugin, IPropertiesPlugin
+from Products.PluggableAuthService.interfaces.plugins import IValidationPlugin
 
 import re
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
-from patch import wrapAllMethods
-from Products.CMFPlone.RegistrationTool import RegistrationTool
-import hashlib
-import random
 import pkg_resources
 try:
     pkg_resources.get_distribution('plone.app.users')
     from plone.app.users.browser.personalpreferences import IPasswordSchema
 except pkg_resources.DistributionNotFound:
     from zope.interface import Interface
+
     class IPasswordSchema(Interface):
         pass
 
@@ -51,9 +36,6 @@ PLUGIN_ID = 'password_strength_plugin'
 PLUGIN_TITLE = 'Create your own rules for enforcing password strength'
 
 
-
-
-
 class RegistrationToolPatch:
     def testPasswordValidity(self, password, confirm=None):
 
@@ -63,11 +45,11 @@ class RegistrationToolPatch:
         o If not, return a string explaining why.
         """
         if confirm is not None and confirm != password:
-            return ( 'Your password and confirmation did not match. '
-                   + 'Please try again.' )
+            return ('Your password and confirmation did not match. '
+                    'Please try again.')
 
         if not password:
-            err = [ 'You must enter a password.' ]
+            err = ['You must enter a password.']
         else:
             err = []
 
@@ -78,21 +60,20 @@ class RegistrationToolPatch:
         for validator_id, validator in validators:
             user = None
             set_id = ''
-            set_info = {'password':password}
-            errors = validator.validateUserInfo( user, set_id, set_info )
-            err += [info['error'] for info in errors if info['id'] == 'password' ]
+            set_info = {'password': password}
+            errors = validator.validateUserInfo(user, set_id, set_info)
+            err += [info['error'] for info in errors if info['id'] == 'password']
         if err:
             return ' '.join(err)
         else:
             return None
 
-wrapAllMethods(RegistrationToolPatch,RegistrationTool)
-
 
 manage_addPasswordStrengthForm = PageTemplateFile(
     'www/passwordStrengthAdd',
     globals(),
-    __name__='manage_addPasswordStrengthForm' )
+    __name__='manage_addPasswordStrengthForm')
+
 
 def manage_addPasswordStrength(dispatcher,
                                id,
@@ -109,13 +90,12 @@ def manage_addPasswordStrength(dispatcher,
                                      'PasswordStrength+plugin+added.'
                                      % dispatcher.absolute_url())
 
-DEFUALT_POLICIES = [(r'.{20}.*','Minimum 20 characters.')
-                    ,(r'.*[A-Z].*','Minimum 1 capital letter.')
-                    ,(r'.*[a-z].*','Minimum 1 lower case letter.')
-                    ,(r'.*[0-9].*','Minimum 1 number.')
-                    ,(r'.*[^0-9a-zA-Z ].*','Minimum 1 non-alpha character.')
+DEFUALT_POLICIES = [(r'.{20}.*', 'Minimum 20 characters.'),
+                    (r'.*[A-Z].*', 'Minimum 1 capital letter.'),
+                    (r'.*[a-z].*', 'Minimum 1 lower case letter.'),
+                    (r'.*[0-9].*', 'Minimum 1 number.'),
+                    (r'.*[^0-9a-zA-Z ].*', 'Minimum 1 non-alpha character.'),
                     ]
-
 
 
 class PasswordStrength(BasePlugin, Cacheable):
@@ -126,80 +106,75 @@ class PasswordStrength(BasePlugin, Cacheable):
     meta_type = 'Password Strength Plugin'
     security = ClassSecurityInfo()
 
-    _properties = ( { 'id'    : 'title'
-                    , 'label' : 'Title'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p1_re'
-                    , 'label' : 'Policy 1 Regular Expression'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p1_err'
-                    , 'label' : 'Policy 1 Error Message'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p2_re'
-                    , 'label' : 'Policy 2 Regular Expression'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p2_err'
-                    , 'label' : 'Policy 2 Error Message'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p3_re'
-                    , 'label' : 'Policy 3 Regular Expression'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p3_err'
-                    , 'label' : 'Policy 3 Error Message'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p4_re'
-                    , 'label' : 'Policy 4 Regular Expression'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p4_err'
-                    , 'label' : 'Policy 4 Error Message'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p5_re'
-                    , 'label' : 'Policy 5 Regular Expression'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  , { 'id'    : 'p5_err'
-                    , 'label' : 'Policy 5 Error Message'
-                    , 'type'  : 'string'
-                    , 'mode'  : 'w'
-                    }
-                  )
-
+    _properties = ({'id': 'title',
+                    'label': 'Title',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p1_re',
+                    'label': 'Policy 1 Regular Expression',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p1_err',
+                    'label': 'Policy 1 Error Message',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p2_re',
+                    'label': 'Policy 2 Regular Expression',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p2_err',
+                    'label': 'Policy 2 Error Message',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p3_re',
+                    'label': 'Policy 3 Regular Expression',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p3_err',
+                    'label': 'Policy 3 Error Message',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p4_re',
+                    'label': 'Policy 4 Regular Expression',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p4_err',
+                    'label': 'Policy 4 Error Message',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p5_re',
+                    'label': 'Policy 5 Regular Expression',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   {'id': 'p5_err',
+                    'label': 'Policy 5 Error Message',
+                    'type': 'string',
+                    'mode': 'w',
+                    },
+                   )
 
     def __init__(self, id, title=None):
         self._id = self.id = id
         self.title = title
 
         i = 1
-        for reg,err in DEFUALT_POLICIES:
+        for reg, err in DEFUALT_POLICIES:
             setattr(self, 'p%i_re' % i, reg)
             setattr(self, 'p%i_err' % i, err)
-            i+=1
-
-
-
+            i += 1
 
     security.declarePrivate('validateUserInfo')
-    def validateUserInfo(self, user, set_id, set_info ):
-
+    def validateUserInfo(self, user, set_id, set_info):
         """ -> ( error_info_1, ... error_info_N )
 
         o Returned values are dictionaries, containing at least keys:
@@ -225,7 +200,7 @@ class PasswordStrength(BasePlugin, Cacheable):
                     errors += [err]
                 i += 1
 
-            errors = [{'id':'password','error':e} for e in errors]
+            errors = [{'id': 'password', 'error': e} for e in errors]
         return errors
 
 
@@ -242,8 +217,10 @@ from zope.schema.interfaces import ValidationError
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 
+
 class CustomPasswordError(ValidationError):
     __doc__ = _("This password doesn't match requirements for passwords")
+
 
 def validate(self, value):
     try:
